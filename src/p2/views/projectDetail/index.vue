84df12a9a8d1c41368a89cc410079b2e034443b6
zhanhui <zhanhui1@ybm100.com>
2020-11-30 15:34:56 +0800

    代码格式化

--- src/p2/views/projectDetail/index.vue
+++ src/p2/views/projectDetail/index.vue
@@ -1,160 +1,180 @@
 <template>
   <div class="detail-wrap">
-     <scroll :data="detail.imgurl" :listenScroll="true" :probeType="3" class="listView" ref="listView" @scroll="onListViewMove">
-        <div class="imgWrap">
-            <el-image v-for="url in detail.imgurl" :key="url" :src="url" :style="drawer?imgWidht:''">
-              <div slot="error" class="image-slot">
-                <i class="el-icon-picture-outline"></i>
-              </div>
-            </el-image>
+    <scroll
+      :data="detail.imgurl"
+      :listenScroll="true"
+      :probeType="3"
+      class="listView"
+      ref="listView"
+      @scroll="onListViewMove"
+    >
+      <div class="imgWrap">
+        <el-image
+          v-for="url in detail.imgurl"
+          :key="url"
+          :src="url"
+          :style="drawer ? imgWidht : ''"
+        >
+          <div slot="error" class="image-slot">
+            <i class="el-icon-picture-outline"></i>
+          </div>
+        </el-image>
+      </div>
+    </scroll>
+    <div class="fixed-title" ref="fixed-title" @click="openDrawer">
+      {{ detail.name }}
+    </div>
+    <div class="drawer" ref="drawer">
+      <div class="drawer-title">
+        <!-- <span>{{detail.name}}</span> -->
+        <i class="el-icon-close" @click="close" />
+      </div>
+      <scroll :listenScroll="true" :probeType="3" class="listView">
+        <div>
+          <div class="abc"></div>
+          <div class="abc"></div>
+          <div class="abc"></div>
+          <div class="abc"></div>
+          <div class="abc"></div>
+          <div class="abc"></div>
         </div>
       </scroll>
-      <div class="fixed-title" ref="fixed-title" @click="openDrawer">{{detail.name}}</div>
-      <div class="drawer" ref="drawer"> 
-          <div class="drawer-title">
-            <!-- <span>{{detail.name}}</span> -->
-            <i class="el-icon-close" @click="close"/> 
-          </div>
-           <scroll :listenScroll="true" :probeType="3" class="listView">
-             <div>
-                <div class="abc"></div>
-                <div class="abc"></div>
-                <div class="abc"></div>
-                <div class="abc"></div>
-                <div class="abc"></div>
-                <div class="abc"></div>
-             </div>
-          </scroll>
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import scroll from '@/components/scroll'
+import scroll from "@/components/scroll";
 export default {
   components: {
-    scroll
+    scroll,
   },
   data() {
     return {
       imgWidht: {
-        width: '70vw'
+        width: "70vw",
       },
-      id: '',
+      id: "",
       detail: {
-        id: '', // 项目id
-        name: '出版项目1', // 项目名称
-        describe: '这是一个牛逼的项目',
+        id: "", // 项目id
+        name: "出版项目1", // 项目名称
+        describe: "这是一个牛逼的项目",
         imgurl: [
-          'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-          'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-          'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-          'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-          'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-          'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-          'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
-      ],
-        videos: []
+          "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+          "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+          "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
+          "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
+          "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
+          "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
+          "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
+        ],
+        videos: [],
       },
-      drawer: false
-    }
+      drawer: false,
+    };
   },
-  created(){
-    this.init()
-    this.$bus.$on('close', (res) => { 
-      if(this.drawer) {
-        this.close()
+  created() {
+    this.init();
+    this.$bus.$on("close", (res) => {
+      if (this.drawer) {
+        this.close();
       }
-    })
+    });
   },
   methods: {
     init() {
-      if(true){console.log(1)}
-      let id = this.$route.query.id || ''
-      this.id = id
-      this.drawer = false
-      this.getDetail()
+      if (true) {
+        console.log(1);
+      }
+      let id = this.$route.query.id || "";
+      this.id = id;
+      this.drawer = false;
+      this.getDetail();
     },
     onListViewMove() {},
-    getDetail() {
-
-    },
+    getDetail() {},
     openDrawer() {
-      if(this.drawer) {
-        return
+      if (this.drawer) {
+        return;
       }
-      this.drawer = true
-      this.$Velocity(this.$refs['fixed-title'],{
-        bottom: function() {
-          return document.body.clientHeight - 30
+      this.drawer = true;
+      this.$Velocity(
+        this.$refs["fixed-title"],
+        {
+          bottom: function () {
+            return document.body.clientHeight - 30;
+          },
+          right: "30px",
         },
-        right: '30px',
-      }, {
-        duration: 450,
-      })
-      this.$Velocity(this.$refs['drawer'], "fadeIn" ,{
-        display: 'block',
-      }, {
-        duration: 450,
-      })
-      
+        {
+          duration: 450,
+        }
+      );
+      this.$Velocity(
+        this.$refs["drawer"],
+        "fadeIn",
+        {
+          display: "block",
+        },
+        {
+          duration: 450,
+        }
+      );
     },
     close() {
-      this.drawer = false
-      this.$Velocity(this.$refs['fixed-title'], 'reverse', {
-        duration: 500
-      })
-      this.$Velocity(this.$refs['drawer'], 'reverse', {
-        duration: 500
-      })
-    }
+      this.drawer = false;
+      this.$Velocity(this.$refs["fixed-title"], "reverse", {
+        duration: 500,
+      });
+      this.$Velocity(this.$refs["drawer"], "reverse", {
+        duration: 500,
+      });
+    },
   },
   watch: {
-      '$route'(to,from) {
-          console.log(this)
-          this.init()
-          this.$forceUpdate()
-      }
-  }
-}
+    $route(to, from) {
+      console.log(this);
+      this.init();
+      this.$forceUpdate();
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.detail-wrap{
-  .listView{
+.detail-wrap {
+  .listView {
     height: 100vh;
     overflow: hidden;
   }
-  /deep/ .el-scrollbar__thumb{
-      background-color: red;
-    }
-   
-   .el-scrollbar{
-      height: 100vh;
-      overflow-x: hidden;
-      .scrollbar-wrap{
-       
-      }
-      
+  /deep/ .el-scrollbar__thumb {
+    background-color: red;
   }
-  /deep/ .el-scrollbar__wrap{
+
+  .el-scrollbar {
+    height: 100vh;
+    overflow-x: hidden;
+    .scrollbar-wrap {
+    }
+  }
+  /deep/ .el-scrollbar__wrap {
     overflow-x: hidden;
   }
-  /deep/ .scrollbar-view{
+  /deep/ .scrollbar-view {
     display: flex;
   }
-  
-  .imgWrap{
+
+  .imgWrap {
     flex: 1;
     font-size: 0;
-    /deep/ .el-image{
+    /deep/ .el-image {
       width: 100%;
     }
   }
-  .imgWrap img{
+  .imgWrap img {
     width: 100vw;
   }
-  .fixed-title{
+  .fixed-title {
     position: fixed;
     right: 0;
     left: 70%;
@@ -165,7 +185,7 @@ export default {
     font-weight: bold;
     z-index: 3;
   }
-  .drawer{
+  .drawer {
     height: 100vh;
     position: fixed;
     right: 0;
@@ -177,15 +197,15 @@ export default {
     // overflow-x:hidden;
     // overflow-y:auto;
   }
-  .abc{
+  .abc {
     height: 300px;
     width: 100%;
     margin-bottom: 10px;
     background: saddlebrown;
   }
-  .drawer-title{
+  .drawer-title {
     text-align: right;
-    i{
+    i {
       display: inline;
     }
   }

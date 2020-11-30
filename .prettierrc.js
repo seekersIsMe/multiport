88868cb9f13cf3b200c526daeb5f11468caaf46f
zhanhui <zhanhui1@ybm100.com>
2020-11-30 15:54:02 +0800

    测试代码格式化

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    printWidth: 150,
+    singleQuote: true,
+    semi: false
+  }
+  
--- src/p2/App.vue
+++ src/p2/App.vue
@@ -1,30 +1,44 @@
 <template>
   <div id="app">
-    <div class="logo">
-     我是logo
-    </div>
+    <div class="logo">我是logo</div>
     <div class="nav">
-      <div class="menuIcon" :class="isShowSup?'fadeLeft':'fadeRight'" @click="showSup">
-      </div>
+      <div class="menuIcon" :class="isShowSup ? 'fadeLeft' : 'fadeRight'" @click="showSup"></div>
       <transition name="slide-fade">
-        <div class="supmenu" v-show="isShowSup" >
-          <div class="supmenuItem" :class="index === activeIndex ? 'active' : ''" v-for="(item,index) in nav" :key="item.id" @click="selectSupMenu(index)">
-              <div class="suppermenuName">
-                {{isCH?item.suppermenuName:item.e_suppermenuName}}
-              </div>
+        <div class="supmenu" v-show="isShowSup">
+          <div
+            class="supmenuItem"
+            :class="index === activeIndex ? 'active' : ''"
+            v-for="(item, index) in nav"
+            :key="item.id"
+            @click="selectSupMenu(index)"
+          >
+            <div class="suppermenuName">
+              {{ isCH ? item.suppermenuName : item.e_suppermenuName }}
             </div>
+          </div>
         </div>
       </transition>
       <transition name="slide-fade">
         <div class="submenu" v-show="isShowSub">
-            <scroll v-for="item in submenu" :key="item.id" :data="item.projectLit" :listenScroll="true" :probeType="3" class="submenuItem listView" ref="listView" @scroll="onListViewMove(item)">
-              <div>
-                  <div class="projectType">{{isCH?item.typeName:item.e_typeName}}</div>
-                  <div v-for="it in item.projectLit" :key="it.id" @click="gotoProjectDetail(it)">
-                    {{it.name}}
-                  </div>
+          <scroll
+            v-for="item in submenu"
+            :key="item.id"
+            :data="item.projectLit"
+            :listenScroll="true"
+            :probeType="3"
+            class="submenuItem listView"
+            ref="listView"
+            @scroll="onListViewMove(item)"
+          >
+            <div>
+              <div class="projectType">
+                {{ isCH ? item.typeName : item.e_typeName }}
               </div>
-            </scroll>
+              <div v-for="it in item.projectLit" :key="it.id" @click="gotoProjectDetail(it)">
+                {{ it.name }}
+              </div>
+            </div>
+          </scroll>
         </div>
       </transition>
     </div>
@@ -32,17 +46,15 @@
       <div class="searchicon" @click="openSearchInput">
         <i class="el-icon-search"></i>
       </div>
-        <div class="lang">
-            <el-dropdown placement="top" trigger="click" @command="selectLang">
-                <span class="el-dropdown-link">
-                {{langText}}<i class="el-icon-arrow-down el-icon--right"></i>
-                </span>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item command="ch">中</el-dropdown-item>
-                  <el-dropdown-item command="en">en</el-dropdown-item>
-                </el-dropdown-menu>
-          </el-dropdown>
-        </div>
+      <div class="lang">
+        <el-dropdown placement="top" trigger="click" @command="selectLang">
+          <span class="el-dropdown-link"> {{ langText }}<i class="el-icon-arrow-down el-icon--right"></i> </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="ch">中</el-dropdown-item>
+            <el-dropdown-item command="en">en</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
     </div>
     <transition name="slide-fade">
       <div class="search-input-wrap" v-show="isShowSearch">
@@ -54,21 +66,20 @@
         ></el-autocomplete>
       </div>
     </transition>
-    <router-view/>
+    <router-view />
   </div>
-
 </template>
 
 <script>
 const LANG = {
-  'ch': '中',
-  'en': 'en'
+  ch: '中',
+  en: 'en',
 }
 import scroll from '@/components/scroll'
 export default {
   name: 'App',
   components: {
-    scroll
+    scroll,
   },
   data() {
     return {
@@ -79,81 +90,81 @@ export default {
       nav: [
         {
           suppermenuName: '项目',
-          e_suppermenuName:'PROJECTS',
+          e_suppermenuName: 'PROJECTS',
           id: '1',
           submenu: [
             {
               typeName: '建筑',
               e_typeName: 'ARCHITECTURE',
-              projectLit: []
-            }, 
+              projectLit: [],
+            },
             {
               typeName: '改造',
               e_typeName: 'RENOVATION',
-              projectLit: []
-            }, 
+              projectLit: [],
+            },
             {
               typeName: '城乡研究',
               e_typeName: 'Urban & rural research',
-              projectLit: []
-            }
-          ]
+              projectLit: [],
+            },
+          ],
         },
         {
           suppermenuName: '展览&演讲',
-          e_suppermenuName:'EXHIBITION&LECTURE',
+          e_suppermenuName: 'EXHIBITION&LECTURE',
           id: '2',
           submenu: [
             {
               typeName: '展览',
               e_typeName: 'exhibition',
-              projectLit: []
-            }, 
+              projectLit: [],
+            },
             {
               typeName: '演讲',
               e_typeName: 'lecture',
-              projectLit: []
-            }
-          ]
+              projectLit: [],
+            },
+          ],
         },
-         {
+        {
           suppermenuName: '出版',
-          e_suppermenuName:'PUBLICATION',
+          e_suppermenuName: 'PUBLICATION',
           id: '3',
           submenu: [
             {
               typeName: '书籍出版',
               e_typeName: 'book',
-              projectLit: []
-            }, 
+              projectLit: [],
+            },
             {
               typeName: '国内刊物',
               e_typeName: 'domestic',
-              projectLit: []
-            }, 
+              projectLit: [],
+            },
             {
               typeName: '国际刊物',
               e_typeName: 'international',
-              projectLit: []
-            }
-          ]
+              projectLit: [],
+            },
+          ],
         },
         {
           suppermenuName: '关于我们',
           isAbout: true,
-          id: '4'
-        }
+          id: '4',
+        },
       ],
       restaurants: [],
       state: '',
-      timeout:  null,
+      timeout: null,
       isShowSearch: false,
       langText: '中',
-      isCH: true
+      isCH: true,
     }
   },
   mounted() {
-     this.restaurants = this.loadAll();
+    this.restaurants = this.loadAll()
   },
   methods: {
     onListViewMove() {
@@ -164,15 +175,15 @@ export default {
       this.isShowSub = false
     },
     selectSupMenu(index) {
-      this.activeIndex = index;
+      this.activeIndex = index
       let item = this.nav[index]
-      if(!item.isAbout) {
+      if (!item.isAbout) {
         this.submenu = this.nav[index].submenu
         this.submenu = this.nav[index].submenu
         this.isShowSub = true
       } else {
         this.$router.push({
-          path: '/aboutUs'
+          path: '/aboutUs',
         })
       }
       this.isShowSup = false
@@ -186,8 +197,8 @@ export default {
         path: '/projectDetail',
         query: {
           id: item.id || '',
-          name: item.name || ''
-        }
+          name: item.name || '',
+        },
       })
     },
     selectLang(e) {
@@ -203,94 +214,159 @@ export default {
     },
     loadAll() {
       return [
-        { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
-        { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
-        { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
-        { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
-        { "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
-        { "value": "贡茶", "address": "上海市长宁区金钟路633号" },
-        { "value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号" },
-        { "value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号" },
-        { "value": "十二泷町", "address": "上海市北翟路1444弄81号B幢-107" },
-        { "value": "星移浓缩咖啡", "address": "上海市嘉定区新郁路817号" },
-        { "value": "阿姨奶茶/豪大大", "address": "嘉定区曹安路1611号" },
-        { "value": "新麦甜四季甜品炸鸡", "address": "嘉定区曹安公路2383弄55号" },
-        { "value": "Monica摩托主题咖啡店", "address": "嘉定区江桥镇曹安公路2409号1F，2383弄62号1F" },
-        { "value": "浮生若茶（凌空soho店）", "address": "上海长宁区金钟路968号9号楼地下一层" },
-        { "value": "NONO JUICE  鲜榨果汁", "address": "上海市长宁区天山西路119号" },
-        { "value": "CoCo都可(北新泾店）", "address": "上海市长宁区仙霞西路" },
-        { "value": "快乐柠檬（神州智慧店）", "address": "上海市长宁区天山西路567号1层R117号店铺" },
-        { "value": "Merci Paul cafe", "address": "上海市普陀区光复西路丹巴路28弄6号楼819" },
-        { "value": "猫山王（西郊百联店）", "address": "上海市长宁区仙霞西路88号第一层G05-F01-1-306" },
-        { "value": "枪会山", "address": "上海市普陀区棕榈路" },
-        { "value": "纵食", "address": "元丰天山花园(东门) 双流路267号" },
-        { "value": "钱记", "address": "上海市长宁区天山西路" },
-        { "value": "壹杯加", "address": "上海市长宁区通协路" },
-        { "value": "唦哇嘀咖", "address": "上海市长宁区新泾镇金钟路999号2幢（B幢）第01层第1-02A单元" },
-        { "value": "爱茜茜里(西郊百联)", "address": "长宁区仙霞西路88号1305室" },
-        { "value": "爱茜茜里(近铁广场)", "address": "上海市普陀区真北路818号近铁城市广场北区地下二楼N-B2-O2-C商铺" },
-        { "value": "鲜果榨汁（金沙江路和美广店）", "address": "普陀区金沙江路2239号金沙和美广场B1-10-6" },
-        { "value": "开心丽果（缤谷店）", "address": "上海市长宁区威宁路天山路341号" },
-        { "value": "超级鸡车（丰庄路店）", "address": "上海市嘉定区丰庄路240号" },
-        { "value": "妙生活果园（北新泾店）", "address": "长宁区新渔路144号" },
-        { "value": "香宜度麻辣香锅", "address": "长宁区淞虹路148号" },
-        { "value": "凡仔汉堡（老真北路店）", "address": "上海市普陀区老真北路160号" },
-        { "value": "港式小铺", "address": "上海市长宁区金钟路968号15楼15-105室" },
-        { "value": "蜀香源麻辣香锅（剑河路店）", "address": "剑河路443-1" },
-        { "value": "北京饺子馆", "address": "长宁区北新泾街道天山西路490-1号" },
-        { "value": "饭典*新简餐（凌空SOHO店）", "address": "上海市长宁区金钟路968号9号楼地下一层9-83室" },
-        { "value": "焦耳·川式快餐（金钟路店）", "address": "上海市金钟路633号地下一层甲部" },
-        { "value": "动力鸡车", "address": "长宁区仙霞西路299弄3号101B" },
-        { "value": "浏阳蒸菜", "address": "天山西路430号" },
-        { "value": "四海游龙（天山西路店）", "address": "上海市长宁区天山西路" },
-        { "value": "樱花食堂（凌空店）", "address": "上海市长宁区金钟路968号15楼15-105室" },
-        { "value": "壹分米客家传统调制米粉(天山店)", "address": "天山西路428号" },
-        { "value": "福荣祥烧腊（平溪路店）", "address": "上海市长宁区协和路福泉路255弄57-73号" },
-        { "value": "速记黄焖鸡米饭", "address": "上海市长宁区北新泾街道金钟路180号1层01号摊位" },
-        { "value": "红辣椒麻辣烫", "address": "上海市长宁区天山西路492号" },
-        { "value": "(小杨生煎)西郊百联餐厅", "address": "长宁区仙霞西路88号百联2楼" },
-        { "value": "阳阳麻辣烫", "address": "天山西路389号" },
-        { "value": "南拳妈妈龙虾盖浇饭", "address": "普陀区金沙江路1699号鑫乐惠美食广场A13" }
-      ];
+        { value: '三全鲜食（北新泾店）', address: '长宁区新渔路144号' },
+        {
+          value: 'Hot honey 首尔炸鸡（仙霞路）',
+          address: '上海市长宁区淞虹路661号',
+        },
+        {
+          value: '新旺角茶餐厅',
+          address: '上海市普陀区真北路988号创邑金沙谷6号楼113',
+        },
+        { value: '泷千家(天山西路店)', address: '天山西路438号' },
+        {
+          value: '胖仙女纸杯蛋糕（上海凌空店）',
+          address: '上海市长宁区金钟路968号1幢18号楼一层商铺18-101',
+        },
+        { value: '贡茶', address: '上海市长宁区金钟路633号' },
+        {
+          value: '豪大大香鸡排超级奶爸',
+          address: '上海市嘉定区曹安公路曹安路1685号',
+        },
+        {
+          value: '茶芝兰（奶茶，手抓饼）',
+          address: '上海市普陀区同普路1435号',
+        },
+        { value: '十二泷町', address: '上海市北翟路1444弄81号B幢-107' },
+        { value: '星移浓缩咖啡', address: '上海市嘉定区新郁路817号' },
+        { value: '阿姨奶茶/豪大大', address: '嘉定区曹安路1611号' },
+        { value: '新麦甜四季甜品炸鸡', address: '嘉定区曹安公路2383弄55号' },
+        {
+          value: 'Monica摩托主题咖啡店',
+          address: '嘉定区江桥镇曹安公路2409号1F，2383弄62号1F',
+        },
+        {
+          value: '浮生若茶（凌空soho店）',
+          address: '上海长宁区金钟路968号9号楼地下一层',
+        },
+        { value: 'NONO JUICE  鲜榨果汁', address: '上海市长宁区天山西路119号' },
+        { value: 'CoCo都可(北新泾店）', address: '上海市长宁区仙霞西路' },
+        {
+          value: '快乐柠檬（神州智慧店）',
+          address: '上海市长宁区天山西路567号1层R117号店铺',
+        },
+        {
+          value: 'Merci Paul cafe',
+          address: '上海市普陀区光复西路丹巴路28弄6号楼819',
+        },
+        {
+          value: '猫山王（西郊百联店）',
+          address: '上海市长宁区仙霞西路88号第一层G05-F01-1-306',
+        },
+        { value: '枪会山', address: '上海市普陀区棕榈路' },
+        { value: '纵食', address: '元丰天山花园(东门) 双流路267号' },
+        { value: '钱记', address: '上海市长宁区天山西路' },
+        { value: '壹杯加', address: '上海市长宁区通协路' },
+        {
+          value: '唦哇嘀咖',
+          address: '上海市长宁区新泾镇金钟路999号2幢（B幢）第01层第1-02A单元',
+        },
+        { value: '爱茜茜里(西郊百联)', address: '长宁区仙霞西路88号1305室' },
+        {
+          value: '爱茜茜里(近铁广场)',
+          address: '上海市普陀区真北路818号近铁城市广场北区地下二楼N-B2-O2-C商铺',
+        },
+        {
+          value: '鲜果榨汁（金沙江路和美广店）',
+          address: '普陀区金沙江路2239号金沙和美广场B1-10-6',
+        },
+        {
+          value: '开心丽果（缤谷店）',
+          address: '上海市长宁区威宁路天山路341号',
+        },
+        { value: '超级鸡车（丰庄路店）', address: '上海市嘉定区丰庄路240号' },
+        { value: '妙生活果园（北新泾店）', address: '长宁区新渔路144号' },
+        { value: '香宜度麻辣香锅', address: '长宁区淞虹路148号' },
+        {
+          value: '凡仔汉堡（老真北路店）',
+          address: '上海市普陀区老真北路160号',
+        },
+        { value: '港式小铺', address: '上海市长宁区金钟路968号15楼15-105室' },
+        { value: '蜀香源麻辣香锅（剑河路店）', address: '剑河路443-1' },
+        { value: '北京饺子馆', address: '长宁区北新泾街道天山西路490-1号' },
+        {
+          value: '饭典*新简餐（凌空SOHO店）',
+          address: '上海市长宁区金钟路968号9号楼地下一层9-83室',
+        },
+        {
+          value: '焦耳·川式快餐（金钟路店）',
+          address: '上海市金钟路633号地下一层甲部',
+        },
+        { value: '动力鸡车', address: '长宁区仙霞西路299弄3号101B' },
+        { value: '浏阳蒸菜', address: '天山西路430号' },
+        { value: '四海游龙（天山西路店）', address: '上海市长宁区天山西路' },
+        {
+          value: '樱花食堂（凌空店）',
+          address: '上海市长宁区金钟路968号15楼15-105室',
+        },
+        { value: '壹分米客家传统调制米粉(天山店)', address: '天山西路428号' },
+        {
+          value: '福荣祥烧腊（平溪路店）',
+          address: '上海市长宁区协和路福泉路255弄57-73号',
+        },
+        {
+          value: '速记黄焖鸡米饭',
+          address: '上海市长宁区北新泾街道金钟路180号1层01号摊位',
+        },
+        { value: '红辣椒麻辣烫', address: '上海市长宁区天山西路492号' },
+        {
+          value: '(小杨生煎)西郊百联餐厅',
+          address: '长宁区仙霞西路88号百联2楼',
+        },
+        { value: '阳阳麻辣烫', address: '天山西路389号' },
+        {
+          value: '南拳妈妈龙虾盖浇饭',
+          address: '普陀区金沙江路1699号鑫乐惠美食广场A13',
+        },
+      ]
     },
     querySearchAsync(queryString, cb) {
-      var restaurants = this.restaurants;
-      var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+      var restaurants = this.restaurants
+      var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants
 
-      clearTimeout(this.timeout);
+      clearTimeout(this.timeout)
       this.timeout = setTimeout(() => {
-        cb(results);
-      }, 3000 * Math.random());
+        cb(results)
+      }, 3000 * Math.random())
     },
     createStateFilter(queryString) {
       return (state) => {
-        return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-      };
+        return state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+      }
     },
     handleSelect(item) {
-      console.log(item);
-    }
+      console.log(item)
+    },
   },
   watch: {
-    isShowSup(newVal,oldVal) {
-      if(newVal) {
+    isShowSup(newVal, oldVal) {
+      if (newVal) {
         this.$bus.$emit('close', true)
       }
     },
-     isShowSub(newVal,oldVal) {
-      if(newVal) {
+    isShowSub(newVal, oldVal) {
+      if (newVal) {
         this.$bus.$emit('close', true)
       }
-    }
-  }
+    },
+  },
 }
-
 </script>
 
 <style lang="less" scoped>
-#app{
+#app {
   position: relative;
-  .logo{
+  .logo {
     width: 200px;
     height: 30px;
     background: sandybrown;
@@ -302,7 +378,7 @@ export default {
     text-align: center;
     z-index: 10;
   }
-  .nav{
+  .nav {
     z-index: 9;
     position: fixed;
     bottom: 10px;
@@ -310,44 +386,45 @@ export default {
     right: 60px;
     height: 30px;
     display: flex;
-    .menuIcon{
+    .menuIcon {
       height: 30px;
       width: 30px;
       background: sandybrown;
       cursor: pointer;
     }
-    .fadeRight{
-       transform: translateX(15px);
-       transition: transform 2s;
+    .fadeRight {
+      transform: translateX(15px);
+      transition: transform 2s;
     }
-    .fadeLeft{
-       transform: translateX(-15px);
-       transition: transform 2s;
+    .fadeLeft {
+      transform: translateX(-15px);
+      transition: transform 2s;
     }
-    .supmenu{
+    .supmenu {
       margin-left: 20px;
       line-height: 30px;
       color: #666;
       display: flex;
       cursor: pointer;
-      .supmenuItem{
+      .supmenuItem {
         margin-left: 15px;
       }
     }
-    .active{
+    .active {
       color: #999;
     }
     .slide-fade-enter-active {
-      transition: all .8s ease;
+      transition: all 0.8s ease;
     }
     .slide-fade-leave-active {
-      transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+      transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
     }
-    .slide-fade-enter, .slide-fade-leave-to {
+    .slide-fade-enter,
+    .slide-fade-leave-to {
       // transform: translateX(10px);
       opacity: 0;
     }
-    .submenu{
+    .submenu {
       position: fixed;
       top: 0;
       bottom: 40px;
@@ -356,18 +433,18 @@ export default {
       display: flex;
       background: white;
       padding: 100px 50px;
-      .submenuItem{
+      .submenuItem {
         margin-right: 30px;
-        .projectType{
+        .projectType {
           line-height: 30px;
         }
       }
-      .submenuItem:last-child{
+      .submenuItem:last-child {
         margin-right: 0;
       }
     }
   }
-  .search-lang-wrap{
+  .search-lang-wrap {
     display: flex;
     z-index: 9;
     position: fixed;
@@ -380,18 +457,18 @@ export default {
     justify-content: space-between;
     padding: 0 10px;
     cursor: pointer;
-    .searchicon{
+    .searchicon {
       padding: 5px;
     }
   }
-  .search-input-wrap{
+  .search-input-wrap {
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 9;
   }
-  .listView{
+  .listView {
     height: calc(100vh - 240px);
   }
 }
